7670ef6a62ddadbb032f515a0380894fceeb1392
Nick Brassel <nick@tzarc.org>
2021-08-01 13:45:07 +1000

    Start reintegrating hid_listen

--- src/electron/electron.js
+++ src/electron/electron.js
@@ -1,23 +1,64 @@
 // electron/electron.js
-const path = require("path");
-const { app, protocol, BrowserWindow } = require("electron");
-const windowStateKeeper = require("electron-window-state");
-const { ipcMain } = require("electron-better-ipc");
+const path = require('path');
+const { app, protocol, BrowserWindow } = require('electron');
+const windowStateKeeper = require('electron-window-state');
+const { ipcMain } = require('electron-better-ipc');
 
 try {
-  require("electron-reloader")(module);
+  require('electron-reloader')(module);
 } catch {}
 
+const HIDDevices = require('./xap/hid-devices');
+const HIDListen = require('./xap/hid-listen');
+
 protocol.registerSchemesAsPrivileged([
-  { scheme: "app", privileges: { secure: true, standard: true } },
+  { scheme: 'app', privileges: { secure: true, standard: true } },
 ]);
 
-const isDev = process.env.IS_DEV == "true" ? true : false;
+const isDev = process.env.IS_DEV == 'true' ? true : false;
+
+///////////
+// Start crappy implementation of hid_listen
+///////////
+
+const hid_devices = HIDDevices.Create();
+const hid_listen = HIDListen(hid_devices);
+
+hid_listen.on('connect', function (d) {
+  ipcMain.callRenderer(win, 'hid_listen-connect', {
+    device: d,
+    timestamp: new Date(),
+  });
+});
+
+hid_listen.on('disconnect', function (d) {
+  ipcMain.callRenderer(win, 'hid_listen-disconnect', {
+    device: d,
+    timestamp: new Date(),
+  });
+});
+
+hid_listen.on('text', function (d, text) {
+  ipcMain.callRenderer(win, 'hid_listen-text', {
+    device: d,
+    timestamp: new Date(),
+    text: text,
+  });
+});
+
+///////////
+// End crappy implementation of hid_listen
+///////////
+
+function quit() {
+  hid_devices.stop();
+  app.quit();
+}
 
 function createWindow() {
-  protocol.registerFileProtocol("app", (request, callback) => {
+  protocol.registerFileProtocol('app', (request, callback) => {
     var url = request.url.substr(6);
-    callback({ path: path.join(__dirname, "../../dist", url) });
+    callback({ path: path.join(__dirname, '../../dist', url) });
   });
 
   const mainWindowState = windowStateKeeper({
@@ -34,7 +75,7 @@ function createWindow() {
     minWidth: 960,
     minHeight: 640,
     webPreferences: {
-      preload: path.join(__dirname, "../preload/preload.js"),
+      preload: path.join(__dirname, '../preload/preload.js'),
       nodeIntegration: false,
       contextIsolation: true,
       allowRunningInsecureContent: false,
@@ -46,7 +87,7 @@ function createWindow() {
 
   // and load the index.html of the app.
   // win.loadFile("index.html");
-  mainWindow.loadURL(isDev ? "http://localhost:3000" : "app://./index.html");
+  mainWindow.loadURL(isDev ? 'http://localhost:3000' : 'app://./index.html');
   // Open the DevTools.
   if (isDev) {
     mainWindow.webContents.openDevTools();
@@ -58,7 +99,9 @@ function createWindow() {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow();
-  app.on("activate", function () {
+  hid_devices.start();
+
+  app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
@@ -68,8 +111,23 @@ app.whenReady().then(() => {
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    quit();
   }
 });
+
+// Exit cleanly on request from parent process in development mode.
+if (isDev) {
+  if (process.platform === 'win32') {
+    process.on('message', (data) => {
+      if (data === 'graceful-exit') {
+        quit();
+      }
+    });
+  } else {
+    process.on('SIGTERM', () => {
+      quit();
+    });
+  }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,11 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import WindiCSS from "vite-plugin-windicss";
-import Components from "vite-plugin-components";
-import ViteIcons, { ViteIconsResolver } from "vite-plugin-icons";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import WindiCSS from 'vite-plugin-windicss';
+import Components from 'vite-plugin-components';
+import ViteIcons from 'vite-plugin-icons';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.ELECTRON == "true" ? "./" : ".",
-  plugins: [
-    vue(),
-    WindiCSS(),
-    Components({
-      customComponentResolvers: ViteIconsResolver(),
-    }),
-    ViteIcons(),
-  ],
+  base: process.env.ELECTRON == 'true' ? './' : '.',
+  plugins: [vue(), WindiCSS(), Components(), ViteIcons()],
 });
