c4d4ac2cadf4f2857ac7fb7e2932b952e4ce9e59
Yan-Fa Li <yanfali@gmail.com>
2021-07-31 21:52:39 -0700

    Add xterm.js for console logging

--- src/renderer/components/HidListen.vue
+++ src/renderer/components/HidListen.vue
@@ -1,20 +1,27 @@
 <template>
   <div>Connected: {{ connects }}, Disconnected: {{ disconnects }}</div>
-  <div v-for="(event, index) in hidMessage" :key="index">
+  <div ref="terminal" id="terminal"></div>
+  <!-- div v-for="(event, index) in hidMessage" :key="index">
     {{ event.timestamp }} {{ event.device.manufacturer }}/{{
       event.device.product
     }}: {{ event.text }}
-  </div>
+  </div-->
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, Ref, onMounted } from 'vue';
+import { ITerminalOptions, Terminal } from 'xterm';
+import 'xterm/css/xterm.css';
 export default defineComponent({
   name: 'HidListen',
   setup() {
     const connects = ref(0);
     const disconnects = ref(0);
     const hidMessage: Ref<Array<any>> = ref([]);
+    const term = new Terminal({
+      cols: 90,
+      scrollback: 500,
+    } as ITerminalOptions);
     onMounted(() => {
       console.log(window.ipc);
       window.ipc.answerMain(
@@ -22,7 +29,9 @@ export default defineComponent({
         (event: HidConnectionEvent) => {
           console.log(event);
           connects.value++;
-          hidMessage.value.push(event);
+          term.write(
+            `${event.timestamp} ${event.device.manufacturer} ${event.device.product}\r\n`
+          );
         }
       );
       window.ipc.answerMain(
@@ -30,13 +39,22 @@ export default defineComponent({
         (event: HidConnectionEvent) => {
           console.log(event);
           disconnects.value++;
-          hidMessage.value.push(event);
+          term.write(
+            `${event.timestamp} ${event.device.manufacturer} ${event.device.product}\r\n`
+          );
         }
       );
       window.ipc.answerMain('hid_listen-text', (event: HidListenTextEvent) => {
         console.log(event);
-        hidMessage.value.push(event);
+        term.write(
+          `${event.timestamp} ${event.device.manufacturer} ${event.device.product}: ${event.text}\r\n`
+        );
       });
+      const terminal = document.getElementById('terminal');
+      if (terminal !== null) {
+        term.open(terminal);
+        term.write('Hello, World!\r\n');
+      }
     });
     return {
       connects,
